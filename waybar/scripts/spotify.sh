680302a21cb6ed916bc9e04b12b514fe0ef00da0
kupa <jedthedog2@gmail.com>
2025-05-30 18:23:19 +1000

    spot update

--- waybar/scripts/spotify.sh
+++ waybar/scripts/spotify.sh
@@ -4,7 +4,7 @@ artist=$(playerctl metadata artist 2>/dev/null)
 title=$(playerctl metadata title 2>/dev/null)
 
 if [ -n "$artist" ] && [ -n "$title" ]; then
-  echo "{\"text\": \"🎧 $artist - $title\", \"class\": \"playing\"}"
+  echo "{\"text\": \" $artist - $title\", \"class\": \"playing\"}"
 else
   echo "{\"text\": \"[ no music ]\", \"class\": \"paused\"}"
 fi
