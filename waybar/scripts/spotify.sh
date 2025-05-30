15e581c831f77b5aab53c523fe67e53d702d503b
kupa <jedthedog2@gmail.com>
2025-05-30 18:18:58 +1000

    spotify_add

---
+++ waybar/scripts/spotify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+artist=$(playerctl metadata artist 2>/dev/null)
+title=$(playerctl metadata title 2>/dev/null)
+
+if [ -n "$artist" ] && [ -n "$title" ]; then
+  echo "{\"text\": \"🎧 $artist - $title\", \"class\": \"playing\"}"
+else
+  echo "{\"text\": \"[ no music ]\", \"class\": \"paused\"}"
+fi
