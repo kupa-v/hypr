dba8e9184843cecadf9ca8e5e75507a335e9663b
kupa <jedthedog2@gmail.com>
2025-05-30 19:39:42 +1000

    Update spotify.sh

--- waybar/scripts/spotify.sh
+++ waybar/scripts/spotify.sh
@@ -2,16 +2,16 @@
 
 # Check if Spotify is running
 if ! pgrep -x spotify >/dev/null; then
-  echo "{\"text\": \"[  ]\", \"class\": \"inactive\"}"
-else
-  # Get metadata
-  artist=$(playerctl metadata artist 2>/dev/null)
-  title=$(playerctl metadata title 2>/dev/null)
+  exit 0
+fi
 
-  # Display formatted output if available
-  if [ -n "$artist" ] && [ -n "$title" ]; then
-    echo "{\"text\": \" [ $title || $artist ]\", \"class\": \"playing\"}"
-  else
-    echo "{\"text\": \"[  no music ]\", \"class\": \"paused\"}"
-  fi
+# Get metadata
+artist=$(playerctl metadata artist 2>/dev/null)
+title=$(playerctl metadata title 2>/dev/null)
+
+# Display formatted output if available
+if [ -n "$artist" ] && [ -n "$title" ]; then
+  echo "{\"text\": \" [ $title || $artist ]\", \"class\": \"playing\"}"
+else
+  echo "{\"text\": \" [ no music ]\", \"class\": \"paused\"}"
 fi
